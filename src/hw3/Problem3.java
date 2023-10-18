196fda551d90b6ab85ddb2c3e9686cc939b75bba
William <we23rt45600@gmail.com>
2023-10-18 18:08:05 +0800

    test3.java

--- src/hw3/Problem3.java
+++ src/hw3/Problem3.java
@@ -25,6 +25,7 @@ public class Problem3 {
 			}
 		}
 		System.out.println("======");
+		System.out.println("======");
 		System.out.print(randomNum.toArray());
 //		for(int i = 1; i < 50; i++) {
 //			if(i % 10 == hateNumber || i / 10 == hateNumber)
