ea919b7c7e5488c6c00e3b667ea47f80a8aceea3
William <we23rt45600@gmail.com>
2023-10-31 13:36:19 +0800

    Version1

--- src/hw4/Problem3.java
+++ src/hw4/Problem3.java
@@ -5,13 +5,26 @@ public class Problem3 {
 //		 有個字串陣列如下 (八大行星)：
 //		 {“mercury”, “venus”, “earth”, “mars”, “jupiter”, “saturn”, “uranus”, “neptune”}
 //		 請用程式計算出這陣列裡面共有多少個母音(a, e, i, o, u) 
-		String[] planets = {"mercury", "venus", "earth", "mars", 
-							"jupiter", "saturn", "uranus", "neptune"};
+		String[] planets = { "mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune" };
 		int sum = 0;
-		for(int i = 0; i < planets.length; i++) {
-			for(char aeiou : planets[i].toCharArray()) {
-				if(aeiou == 'a' || aeiou == 'e' || aeiou == 'i' || aeiou == 'o' || aeiou == 'u') {
-					sum++;
+		for (int i = 0; i < planets.length; i++) {
+			for (char aeiou : planets[i].toCharArray()) {
+				switch (aeiou) {
+				case 'a':
+					sum += 1;
+					break;
+				case 'e':
+					sum += 1;
+					break;
+				case 'i':
+					sum += 1;
+					break;
+				case 'o':
+					sum += 1;
+					break;
+				case 'u':
+					sum += 1;
+					break;
 				}
 			}
 		}
