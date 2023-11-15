5bb99e56ec1c3a7a927f405c5f69e32955357175
William <we23rt45600@gmail.com>
2023-11-15 22:07:59 +0800

    Version1

--- src/hw10/Problem3.java
+++ src/hw10/Problem3.java
@@ -1,7 +1,8 @@
 package hw10;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Problem3 {
@@ -10,40 +11,44 @@ public class Problem3 {
 //		 可以轉成想要的輸出格式化成(1)年/月/日(2)月/日/年(3)日/月/年三選一
 //		 ，而輸入非指定日期數字格式會顯示出提示訊息如圖
 
-		String userDate, choose;
+		String userDate, choose, showDate;
 		String dateRule = "^\\d{8}$";
 		String chooseRule = "^[1-3]$";
 		Scanner sc = new Scanner(System.in);
 		int year, month, day;
+		
+		//日期輸入和判斷
 		do {
 			System.out.print("請輸入日期(年月日，例如:20110131)：");
 			userDate = sc.next();
 			year = Integer.parseInt(userDate.substring(0, 4));
 			month = Integer.parseInt(userDate.substring(4, 6));
 			day = Integer.parseInt(userDate.substring(6));
-		} while (!matchNum(userDate, dateRule) || !matchDate(year, month, day));
-
-		Calendar date = new GregorianCalendar(year, month - 1, day);
-
+		} while (!matchNum(userDate, dateRule) || ((showDate = matchDate(year, month, day, "yyyy/MM/dd")) == null));
+		
+		//選擇欲格式化選項
 		do {
 			System.out.print("欲格式化成(1)年/月/日(2)月/日/年(3)日/月/年：");
 			choose = sc.next();
 		} while (!matchNum(choose, chooseRule));
-
+		
+		//顯示選擇結果
 		switch (Integer.parseInt(choose)) {
 		case 1:
-			System.out.printf("年/月/日：%tY/%<tm/%<td", date);
+			System.out.println(showDate);
 			break;
 		case 2:
-			System.out.printf("月/日/年：%tm/%<td/%<tY", date);
+			System.out.println(matchDate(year, month, day, "MM/dd/yyyy"));
 			break;
 		case 3:
-			System.out.printf("日/月/年：%td/%<tm/%<tY", date);
+			System.out.println(matchDate(year, month, day, "dd/MM/yyyy"));
 			break;
+			
 		}
 		sc.close();
 	}
 
+	//使用者輸入長度判斷
 	public static boolean matchNum(String num, String regex) {
 		if (num.matches(regex))
 			return true;
@@ -53,19 +58,17 @@ public class Problem3 {
 		}
 	}
 
-	public static boolean matchDate(int year, int month, int day) {
-		GregorianCalendar gc = new GregorianCalendar(year, month - 1, day);
-		if (year < 0 || month < 1 || month > 12 || day < 1 || day > 31) {
-			System.out.println("日期輸入錯誤，請重新輸入！");
-		} else if (gc.isLeapYear(year) && month == 2 && day > 29) {
-			System.out.println("潤年2月只有到29號，請重新輸入！");
-		} else if (!gc.isLeapYear(year) && month == 2 && day > 28) {
-			System.out.println("平年2月只有到28號，請重新輸入！");
-		} else if (day > 30 && !(month % 7 == 0 || (month % 7) % 2 == 1)) {
-			System.out.println("小月只有到30號，請重新輸入！");
-		} else {
-			return true;
+	//判斷日期是否符合
+	public static String matchDate(int year, int month, int day, String format) {
+		try {
+			String date = LocalDate.of(year, month, day)
+							.format(DateTimeFormatter.ofPattern(format));
+			return date;
+			
+		}catch(DateTimeException e) {
+			System.out.println("日期格式不正確，請再輸入一次！");
+			return null;
 		}
-		return false;
 	}
+	
 }
